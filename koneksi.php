635b96ead33a08cde1bde0b5f2bf5e7b2380bf07
frado <fradoforumm@gmail.com>
2021-05-10 20:39:27 +0700

    Hapus

--- koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
- define('HOST','localhost');
- define('USER','(maaf, karena saya cuma memakai android, ini cuma code miniatur)');
- define('PASS','');
- define('DB','Buku');
- 
- $con = mysqli_connect(HOST,USER,PASS,DB) or die('Tidak dapat terhubung dong, kan cuma miniatur, hehe');
- ?>
