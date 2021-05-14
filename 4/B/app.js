5757ada458823e9515f684c79b87f3a4576c117e
frado <fradoforumm@gmail.com>
2021-05-14 15:40:30 +0700

    Selesai

--- 4/B/app.js
+++ 4/B/app.js
@@ -24,14 +24,93 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 
 app.get('/', (req, res) => {
-    let sql = "SELECT * FROM books";
-    let query = connection.query(sql, (err, rows) => {
+    let sqlC = "SELECT * FROM categories";
+    let sqlB = "SELECT * FROM books";
+    let query = connection.query(sqlC, (err, rowsC) => {
         if(err) throw err;
-        res.render('user_index', {
-            title: 'CRUD Dumbways',
-            books: rows
+        let query = connection.query(sqlB, (err, rowsB) => {
+            if(err) throw err;
+            res.render('index', {
+                title: 'CRUD Dumbways',
+                categories: rowsC,
+                books: rowsB
+            });
         });
     });
+});
+
+app.get('/tambah-kategori', (req, res) => {
+    res.render('tambah-kategori', {
+        title: 'CRUD Dumbways'
+    });
+});
+
+app.get('/tambah-buku', (req, res) => {
+    let sqlC = "SELECT * FROM categories";
+    let query = connection.query(sqlC, (err, rowsC) => {
+        if(err) throw err;
+        res.render('tambah-buku', {
+            title: 'CRUD Dumbways',
+            categories: rowsC
+        });
+    });
+});
+
+app.post('/save-kategori', (req, res) => {
+    let data = {name: req.body.name,}
+    let sql = "INSERT INTO categories SET ?";
+    let query = connection.query(sql, data, (err, result) => {
+        if(err) throw err;
+        res.redirect('/');
+    });
+});
+
+app.post('/save-buku', (req, res) => {
+    let data = {name: req.body.name, category_id: req.body.category_id, stok: req.body.stok, image: req.body.gambar}
+    let sql = "INSERT INTO books SET ?";
+    let query = connection.query(sql, data, (err, result) => {
+        if(err) throw err;
+        res.redirect('/');
+    });
+});
+
+app.get('/edit-buku/:bookId', (req, res) => {
+    const bookId = req.params.bookId;
+    let sql = `SELECT * FROM books WHERE id = ${bookId}`;
+    let query = connection.query(sql, (err, result) => {
+        if(err) throw err;
+        res.render('edit-buku', {
+            title: 'CRUD Dumbways',
+            book: result[0]
+        });
+    }); 
+});
+
+app.post('/update-buku', (req, res) => {
+    const bookId = req.body.id;
+    let sql = "UPDATE books SET name='" +req.body.name+ "', stok='" +req.body.stok+ "', image='" +req.body.gambar+"' WHERE id = " +bookId;
+    let query = connection.query(sql, (err, result) => {
+        if(err) throw err;
+        res.redirect('/');
+    });
+});
+
+app.get('/delete-buku/:bookId', (req, res) => {
+    const bookId = req.params.bookId;
+    let sql = `DELETE FROM books WHERE id = ${bookId}`;
+    let query = connection.query(sql, (err, result) => {
+        if(err) throw err;
+        res.redirect('/');
+    }); 
+});
+
+app.get('/delete-kategori/:categoryId', (req, res) => {
+    const categoryId = req.params.categoryId;
+    let sql = `DELETE FROM categories WHERE id = ${categoryId}`;
+    let query = connection.query(sql, (err, result) => {
+        if(err) throw err;
+        res.redirect('/');
+    }); 
 });
 
 app.listen(3000, () => {
